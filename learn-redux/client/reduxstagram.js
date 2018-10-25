e42e91e9923d624b7a9b0116738dd28a935de6f8
Daniel Lousa <dlousa92@gmail.com>
2018-10-24 20:49:40 -0400

    render Main on load

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import  { render } from 'react-dom'
 import css from './styles/style.styl'
-render(<p>heyoo</p>, document.getElementById('root'))
 
+import Main from './components/Main'
 
+render(<Main />, document.getElementById('root'))
