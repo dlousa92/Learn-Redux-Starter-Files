803aa7739e1709662103b2f34362ad3a5b08265d
Daniel Lousa <dlousa92@gmail.com>
2018-10-25 12:57:35 -0400

    connect react and redux

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
 import css from './styles/style.styl'
 
 import Main from './components/Main'
@@ -8,12 +10,14 @@ import Photogrid from './components/Photogrid'
 import Single from './components/single'
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path='/' component={Main}>
-      <IndexRoute component={Photogrid} />
-      <Route path='/view/:postId' component={Single} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path='/' component={Main}>
+        <IndexRoute component={Photogrid} />
+        <Route path='/view/:postId' component={Single} />
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'))
