6c612532f9131945aaa095e59708c9dd487b0451
Daniel Lousa <dlousa92@gmail.com>
2018-10-29 13:54:47 -0400

    create Comment component

---
+++ learn-redux/client/components/Comment.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Comment = React.createClass({
+  render () {
+
+    return (
+      <div class='comment'>
+        I'm a comment
+      </div>
+    )
+  }
+})
+
+export default Comment
