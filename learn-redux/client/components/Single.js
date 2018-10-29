35eca8633bbc9a3e8fbb98efc26623ad25a0c069
Daniel Lousa <dlousa92@gmail.com>
2018-10-29 13:55:26 -0400

    show individual comments and photo when clicking on a post

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Photo from './Photo'
+import Comment from './Comment'
 
 const Single = React.createClass({
   render () {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i]
     return (
       <div className='single-photo'>
-        Im the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comment />
       </div>
     )
   }
